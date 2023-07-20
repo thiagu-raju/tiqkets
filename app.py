763efd458d64c3ce10ceb93d0ebf972c336cf963
thiagu-raju <107220289+thiagu-raju@users.noreply.github.com>
2023-07-20 21:10:01 +0000

    Signup Page

--- app.py
+++ app.py
@@ -1,10 +1,47 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import mysql.connector
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_word():
-  return render_template("home.html")
+# Replace these credentials with your own MySQL database credentials
+db_credentials = {
+    'host': 'your-hostname',
+    'user': 'your-username',
+    'password': 'your-password',
+    'database': 'your-database',
+}
 
-if __name__ == "__main__":
-  app.run(host='0.0.0.0', debug=True)
+# Route for the signup page
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        role = request.form['role']
+
+        # Insert user details into the database
+        insert_user(username, password, role)
+
+        return "Signup Successful! You can now log in."
+    
+    return render_template('signup.html')
+
+# Function to insert user details into the database
+def insert_user(username, password, role):
+    try:
+        connection = mysql.connector.connect(**db_credentials)
+        cursor = connection.cursor()
+
+        # SQL query to insert user details into the table
+        query = "INSERT INTO tblUsers (user_name, password, role) VALUES (%s, %s, %s)"
+        values = (username, password, role)
+        cursor.execute(query, values)
+
+        connection.commit()
+        cursor.close()
+        connection.close()
+    except Exception as e:
+        print("Error:", e)
+
+if __name__ == '__main__':
+    app.run(debug=True)
