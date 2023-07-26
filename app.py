d64775e38ae8498155b0d7a28e8beaf3e4f2565f
thiagu-raju <107220289+thiagu-raju@users.noreply.github.com>
2023-07-26 19:51:45 +0000

    fixed bugs

--- app.py
+++ app.py
@@ -87,4 +87,4 @@ def register():
     return render_template('register.html', message=message)
 
 if __name__ == "__main__":
-    app.run(host ='0.0.0.0', debug=True)
+    app.run(host ='127.0.0.1', debug=True)
